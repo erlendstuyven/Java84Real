59a0a592d3bcd3ff1e5b5996bb012d61ec8bf02d
erlendstuyven <erlend.stuyven@gmail.com>
2017-01-01 19:52:29 +0100

    commit codewars pokerhand

--- src/main/java/codewars/PokerHand.java
+++ src/main/java/codewars/PokerHand.java
@@ -4,53 +4,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-/**
- * Created by BKERL on 31/12/2016.
- */
-public class PokerHand {
-    public static boolean IsStraight(List<Integer> hand) {
-        Collections.sort(hand);
-        List<Integer> handWhereAcesAreReplacedByOnes = replaceAcesByOnes(hand);
-        return isStraight(hand) || isStraight(handWhereAcesAreReplacedByOnes);
-    }
+public final class PokerHand {
 
-    private static List<Integer> replaceAcesByOnes(List<Integer> hand) {
-        List<Integer> handWhereAcesAreReplacedByOnes = new ArrayList<>();
-        if (containsAnAce(hand)){
-            handWhereAcesAreReplacedByOnes = hand.stream().map(integer -> {
-                if (integer == 14) {
-                    return 1;
-                } else {
-                    return integer;
-                }
-            }).collect(Collectors.toList());
-            Collections.sort(handWhereAcesAreReplacedByOnes);
+    public static boolean IsStraight(ArrayList<Integer> cards) {
+        if (cards.size() < 5) {return false;}
+        addCardOneWhenAceDedected(cards);
+        List<Integer> cardsList = cards.stream().distinct().sorted().collect(Collectors.toList());
+        if (isStraight(cardsList) >=5){
+            return true;
         }
-        return handWhereAcesAreReplacedByOnes;
-    }
+        return false;
+        }
 
-    private static boolean containsAnAce(List<Integer> hand) {
-        long aceCount = hand.stream().filter(integer -> integer == 14).count();
-        return (aceCount >= 1);
-    }
-
-    private static boolean isStraight(List<Integer> hand) {
-        boolean isStraight = false;
-        int straightCounter = 1;
-        for (int i = 0; i < hand.size(); i++) {
-            Integer integer =  hand.get(i);
-            if (i>0) {
-                if ((integer == hand.get(i-1) + 1) || (integer == hand.get(i-1))){
-                    straightCounter++;
-                } else {
-                    straightCounter=1;
-                }
-                if (straightCounter == 5) {
-                    isStraight = true;
-                }
+    private static int isStraight(List<Integer> cardsList) {
+        int straightCounter=1;
+        for (int i = 0; i < cardsList.size()-1; i++) {
+            if (cardsList.get(i) == cardsList.get(i + 1) - 1) {
+                straightCounter++;
             }
         }
-        return isStraight;
+        return straightCounter;
+    }
+
+    private static void addCardOneWhenAceDedected(ArrayList<Integer> cards) {
+        if(cards.stream().anyMatch(i->i==14)){
+            cards.add(1);
+        }
     }
 }
+
+
