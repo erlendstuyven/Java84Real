df74b00c661656ac31a29e6898ab77f3065ad6a6
erlendstuyven <erlend.stuyven@gmail.com>
2017-01-01 21:13:40 +0100

    commit codewars digitalroot

--- src/main/java/codewars/DigitalRoot.java
+++ src/main/java/codewars/DigitalRoot.java
@@ -1,33 +1,21 @@
 package codewars;
 
-/**
- * Created by BKERL on 31/12/2016.
- */
 public class DigitalRoot {
 
     public static int digital_root(int incomingNumber) {
         int root = 0;
-        root = calculateRoot(incomingNumber, root);
-        root = recalculateRootWhenGreaterThan10(root);
+        root = calculateDigitalRoot(incomingNumber, root);
         return  root;
     }
 
-    private static int calculateRoot(int incomingNumber, int root) {
-        for (char digit : getChars(incomingNumber)) {
+    private static int calculateDigitalRoot(int incomingNumber, int root) {
+        for (char digit : String.valueOf(incomingNumber).toCharArray()) {
             root = root + Integer.parseInt(Character.toString(digit));
         }
-        return root;
-    }
-
-    private static int recalculateRootWhenGreaterThan10(int root) {
         if (root > 10) {
             return digital_root(root);
         }
         return root;
     }
 
-    private static char[] getChars(int n) {
-        String numberAsString = String.valueOf(n);
-        return numberAsString.toCharArray();
-    }
 }
